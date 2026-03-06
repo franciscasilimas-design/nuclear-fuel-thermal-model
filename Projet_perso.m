2b704c40025bda94de8faeea68476ffdef6c9802
franciscasilimas-design <franciscasilimas@gmail.com>
2026-03-06 15:02:08 +0100

    Add files via upload

--- Projet_perso.m
+++ Projet_perso.m
@@ -126,9 +126,12 @@ Ts = T1(end);   % température à r = R1
 %% Profil non-linéaire (Kirchhoff)
 
 Tc_linear = T1(1);
-Tc_nonlinear = T_nl(1);
+
 
 T_nl = ( (a + b*Ts).*exp( (b*qppp/4)*(R1^2 - r1.^2) ) - a )/b;
+
+Tc_nonlinear = T_nl(1);
+
 k_center = 1/(a + b*Tc_nonlinear);
 
 
@@ -228,29 +231,28 @@ disp(['k_center non-linéaire = ', num2str(k_center)])
 
 
 % Puissance volumique (MW/m^3)
-H = [100 200 300 400 500];
+%H = [100 200 300 400 500];
 
 % Température centrale modèle linéaire (K)
-Tc_lin = [864.6 1147.9 1431.4 1715.4 1999.4];
+%Tc_lin = [864.6 1147.9 1431.4 1715.4 1999.4];
 
 % Température centrale modèle non-linéaire (K)
-Tc_nl = [821.5 1088.5 1384.5 1709.9 2069.1];
+%Tc_nl = [821.5 1088.5 1384.5 1709.9 2069.1];
 
 % Tracé
-figure
+%figure
 
-plot(H,Tc_lin,'-o','LineWidth',2)
-hold on
+%plot(H,Tc_lin,'-o','LineWidth',2)
+%hold on
 
-plot(H,Tc_nl,'-s','LineWidth',2)
+%plot(H,Tc_nl,'-s','LineWidth',2)
+%grid on
 
-grid on
+%xlabel('Puissance volumique H (MW/m^3)','FontSize',12)
+%ylabel('Température centrale T_c (K)','FontSize',12)
 
-xlabel('Puissance volumique H (MW/m^3)','FontSize',12)
-ylabel('Température centrale T_c (K)','FontSize',12)
+%title('Température centrale en fonction de la puissance volumique','FontSize',14)
 
-title('Température centrale en fonction de la puissance volumique','FontSize',14)
-
-legend('Modèle linéaire','Modèle non-linéaire','Location','northwest')
+%legend('Modèle linéaire','Modèle non-linéaire','Location','northwest')
 
 
